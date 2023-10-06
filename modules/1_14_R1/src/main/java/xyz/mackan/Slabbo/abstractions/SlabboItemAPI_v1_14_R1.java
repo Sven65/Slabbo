5218ac75c24542f41f8afb4ee5b2e8ce25dc9519
Sven65 <Sven65@users.noreply.github.com>
2023-10-06 11:38:14 +0200

    Add custom model data for Abstracted GUI items in 1.14+

--- modules/1_14_R1/src/main/java/xyz/mackan/Slabbo/abstractions/SlabboItemAPI_v1_14_R1.java
+++ modules/1_14_R1/src/main/java/xyz/mackan/Slabbo/abstractions/SlabboItemAPI_v1_14_R1.java
@@ -3,40 +3,49 @@ package xyz.mackan.Slabbo.abstractions;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import xyz.mackan.Slabbo.Slabbo;
 
 public class SlabboItemAPI_v1_14_R1 implements SlabboItemAPI {
 	public SlabboItemAPI_v1_14_R1 () {}
 
-	public ItemStack getRedStainedGlassPane () {
-		return new ItemStack(Material.RED_STAINED_GLASS_PANE, 1);
+	private ItemStack addCustomModelData(ItemStack stack, String name) {
+		ItemMeta meta = stack.getItemMeta();
+		meta.setCustomModelData(Slabbo.getInstance().getConfig().getInt("items.customModelData." + name, 0));
+		stack.setItemMeta(meta);
+
+		return stack;
 	}
 
-	public ItemStack getGreenStainedGlassPane () {
-		return new ItemStack(Material.GREEN_STAINED_GLASS_PANE, 1);
+	public ItemStack getRedStainedGlassPane () {
+		return this.addCustomModelData(new ItemStack(Material.RED_STAINED_GLASS_PANE, 1), "redStainedPane");
 	}
 
 	public ItemStack getYellowStainedGlassPane () {
-		return new ItemStack(Material.YELLOW_STAINED_GLASS_PANE, 1);
+		return this.addCustomModelData(new ItemStack(Material.YELLOW_STAINED_GLASS_PANE, 1), "yellowStainedPane");
+	}
+
+	public ItemStack getGreenStainedGlassPane () {
+		return this.addCustomModelData(new ItemStack(Material.GREEN_STAINED_GLASS_PANE, 1), "greenStainedPane");
 	}
 
 	public ItemStack getLimeStainedGlassPane () {
-		return new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1);
+		return this.addCustomModelData(new ItemStack(Material.LIME_STAINED_GLASS_PANE, 1), "limeStainedPane");
 	}
 
 	public ItemStack getCommandBlock () {
-		return new ItemStack(Material.COMMAND_BLOCK, 1);
+		return this.addCustomModelData(new ItemStack(Material.COMMAND_BLOCK, 1), "commandBlock");
 	}
-
 	public ItemStack getChestMinecart () {
-		return new ItemStack(Material.CHEST_MINECART, 1);
+		return this.addCustomModelData(new ItemStack(Material.CHEST_MINECART, 1), "chestMinecart");
 	}
 
 	public ItemStack getComparator () {
-		return new ItemStack(Material.COMPARATOR, 1);
+		return this.addCustomModelData(new ItemStack(Material.COMPARATOR, 1), "comparator");
 	}
 
 	public ItemStack getOakSign () {
-		return new ItemStack(Material.OAK_SIGN, 1);
+		return this.addCustomModelData(new ItemStack(Material.OAK_SIGN, 1), "oakSign");
 	}
 
 	public ItemStack getDefaultSlab () { return new ItemStack(Material.STONE_SLAB, 1); }
