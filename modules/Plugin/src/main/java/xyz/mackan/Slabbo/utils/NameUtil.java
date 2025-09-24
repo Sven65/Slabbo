43613f05539e44e7061d961c1f1135497dcc7d0e
Sven65 <Sven65@users.noreply.github.com>
2025-09-24 12:50:01 +0200

    fix: display custom item names in admin interface

--- modules/Plugin/src/main/java/xyz/mackan/Slabbo/utils/NameUtil.java
+++ modules/Plugin/src/main/java/xyz/mackan/Slabbo/utils/NameUtil.java
@@ -7,17 +7,18 @@ import org.bukkit.inventory.meta.ItemMeta;
 import xyz.mackan.Slabbo.abstractions.SlabboAPI;
 
 public class NameUtil {
-
-	public static String getName (ItemStack is) {
+	public static String getName(ItemStack is) {
+		if (is == null) return "";
 		ItemMeta meta = is.getItemMeta();
-		boolean hasDisplayName = meta != null && meta.hasDisplayName();
-
-		// return displayname if item has one
-		if(is.hasItemMeta() && hasDisplayName) return is.getItemMeta().getDisplayName();
-
+		if (meta != null) {
+			if (meta.hasDisplayName() && !meta.getDisplayName().isEmpty()) {
+				return meta.getDisplayName();
+			}
+			if (meta.hasLocalizedName() && meta.getLocalizedName() != null && !meta.getLocalizedName().isEmpty()) {
+				return meta.getLocalizedName();
+			}
+		}
 		SlabboAPI api = Bukkit.getServicesManager().getRegistration(SlabboAPI.class).getProvider();
-
 		return api.getItemName(is);
 	}
-
 }
